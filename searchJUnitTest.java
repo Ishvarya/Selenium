aeca3d2772bc824a86121cdeee48480e1f3566d8
Ishvarya <contact.ishvarya@gmail.com>
2018-06-07 16:00:44 +0200

    commit;
    commit

--- searchJUnitTest.java
+++ searchJUnitTest.java
@@ -2,12 +2,16 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
+
+import junit.framework.Assert;
 
 public class searchJUnitTest {
 	@BeforeClass
@@ -22,6 +26,9 @@ public class searchJUnitTest {
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\ishvarya\\Selenium\\SeleniumDrivers\\chromedriver_win32\\chromedriver.exe");
 	}
 	
+	@Rule
+	public ErrorCollector ec = new ErrorCollector();
+	
 	@Test
 	public void searchText() {
 		System.out.println("Inside search Block");
@@ -33,6 +40,19 @@ public class searchJUnitTest {
 		searchText.sendKeys("Charles Babbage");
 		WebElement searchButton = chromeDriver.findElement(By.className("sprite svg-search-icon"));
 		searchButton.click();
+		
+		
+		String expected_title = "Wiki";
+		String actual_title = chromeDriver.getTitle();
+		try {
+			//used to fail a test case using comparison
+			Assert.assertEquals(expected_title, actual_title);
+		}
+		catch(Throwable t){
+			
+			System.out.println("Error captured");
+			ec.addError(t);
+		}
 	}
 	
 	@After
